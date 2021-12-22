65ca98ad25f94a5aa04efa9fdbedca0af007a880
Moratorium1 <73707061+Moratorium1@users.noreply.github.com>
2021-12-22 14:30:14 +0000

    Animation blueprint implemented
    ABP_ThirdPersonCharacter event graph and statemachine setup
    Note: Crouch does not seem to function

---
+++ Source/CO2301_Assignment/ThirdPersonGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ThirdPersonGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CO2301_ASSIGNMENT_API AThirdPersonGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
